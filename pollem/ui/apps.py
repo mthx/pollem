ccd799c5ce042548b4cea9515bf766d8418439b5
Matt Hillsdon <mth@corefiling.com>
2018-07-17 09:07:17 +0100

    Aspirational README.
    Remove UI app.

--- pollem/ui/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PollemUiConfig(AppConfig):
-    name = 'pollem.ui'
-    app_label = 'ui'
--- pollem/urls.py
+++ pollem/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/', include(router.urls)),
-    url(r'^/', include('pollem.ui.urls')),
 ]
