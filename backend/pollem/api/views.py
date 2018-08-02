ed3dffc046ad00ae684795785bbe9d30b51d4e05
Matt Hillsdon <matt@hillsdon.net>
2018-08-02 13:13:34 +0100

    Remove debug logging.

--- backend/pollem/api/views.py
+++ backend/pollem/api/views.py
@@ -4,13 +4,9 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
-import logging
 
 from .models import Poll, Choice
 from .serializers import PollSerializer, ChoiceSerializer
-
-
-logger = logging.getLogger(__name__)
 
 
 class PollViewSet(viewsets.ModelViewSet):
@@ -22,7 +18,6 @@ class PollViewSet(viewsets.ModelViewSet):
         choice_id = request.data['choice']
         
         if choice_id:
-            logger.info('Got {}'.format(choice_id))
             updated = Choice.objects.filter(pk=choice_id).update(votes=F('votes') + 1)
             if updated:
                 return self.retrieve(pk);
